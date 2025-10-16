1c5e70c14f6a9abc286790adb45dc7548aa7520d
falcaovideomaker <falcao.videomaker@gmail.com>
2025-10-16 16:20:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='metaxp-2.3'; const ASSETS=['./','./index.html','./app.js','./manifest.json'];
+const CACHE='metaxp-2.4'; const ASSETS=['./','./index.html','./app.js','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))) });
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
