22a099ef62efffe8e8bfc546e0340e3d9f23f33a
falcaovideomaker <falcao.videomaker@gmail.com>
2025-10-16 16:51:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-const CACHE='metaxp-2.3'; const ASSETS=['./','./index.html','./app.js','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))) });
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+// sw.js – v2.3.1
+const CACHE = 'metaxp-2.3.1';
+const ASSETS = ['./', './index.html', './app.js?v=2.3.1', './manifest.json?v=2.3.1'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
